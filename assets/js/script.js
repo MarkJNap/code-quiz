25977a15abd501be2354a4259854c793d83d3cea
Mark Napolitano <156marknap@gmail.omgit config --global user.name oMarkNx>
2022-12-07 18:35:25 +0800

    added a correct or wrong display, added correct or wrong classes in css for visual aid, in js added questions into an array with the answers correct being true, made timer have text when at 0 seconds, added code for displaying questions and for identifying answers, added code for moving to next question and if all out of questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,167 @@
-// ------What do I need to have happen?------
 const timerEl = document.getElementById("timer")
 const startButtonEl = document.getElementById("start-button")
 const startTextEl = document.getElementById("start-text")
 const questionsContainerEl = document.getElementById("question-container")
 const questionEl = document.getElementById("question")
 const answerButtonsEl = document.getElementById("answer-buttons")
+const selectedAnswerEl = document.getElementById("answer-display")
+const questionsAll = [
+    {
+        question: "In JavaScript, What is the type of loop that continues through a block of code as long as the specified condition remains TRUE?",
+        answers: [
+            { text: "Else", correct: false},
+            { text: "While", correct: true},
+            { text: "For", correct: false},
+            { text: "Conditional", correct: false}
+        ]
+    },    
+    {
+        question: "What HTML element is a container for elements that may include the document title, scripts, styles, meta information, and more?",
+        answers: [
+            { text: "<head>", correct: true},
+            { text: "<title>", correct: false},
+            { text: "<a>", correct: false},
+            { text: "<header>", correct: false}
+        ]
+    },  
+    {
+        question: "In JavaScript, what element is used to store and manipulate text?",
+        answers: [
+            { text: "Variables", correct: false},
+            { text: "Booleans", correct: false},
+            { text: "Strings", correct: true},
+            { text: "Methods", correct: false}
+        ]
+    },   
+    {
+        question: "What does HTML stand for?",
+        answers: [
+            { text: "Home Tool Markup Language", correct: false},
+            { text: "Hyperlinks and Text Markup Language", correct: false},
+            { text: "Hyper Tools and Making Language", correct: false},
+            { text: "Hyper Text Markup Language", correct: true}
+        ]
+    },    
+    {
+        question: "What is the box called in CSS that wraps around every HTML element?",
+        answers: [
+            { text: "Box-model", correct: true},
+            { text: "Float", correct: false},
+            { text: "Wrap", correct: false},
+            { text: "Boundary", correct: false}
+        ]
+    },    
+    {
+        question: "How do you display a border like this: The top border = 10 pixels, The bottom border = 5 pixels, The left border = 20 pixels, The right border = 2 pixels?",
+        answers: [
+            { text: "border-width:10px 2px 5px 20px;", correct: true},
+            { text: "border-width:5px 20px 10px 2px;", correct: false},
+            { text: "border-width:10px 20px 5px 2px;", correct: false},
+            { text: "border-width:10px 5px 20px 2px;", correct: false}
+        ]
+    }
+]
+var currentQuestion = 0
+var secondsLeft = 100;
 
-// Put questions into here in an array? or have each question as a const to use...
-const questions = "Quiz(Temp)"
-
-// -----User needs to start the quiz
+// ------What do I need to have happen?------
+// -----User clicks start the quiz
 startButtonEl.addEventListener("click", startGame)
 
+// -----What happens when the game starts
 function startGame() {
     startButtonEl.classList.add("hide")
     startTextEl.classList.add("hide")
+    selectedAnswerEl.classList.add("hide")
     questionsContainerEl.classList.remove("hide")
     timerStart()
     setQuestion()
 }
 
-// -----Quiz needs questions popualted
-function setQuestion() {
-
-}
-// -----Timer needs to countdown
+// -----Timer begins countdown
 function timerStart() {
-    var secondsLeft = 100;
     var timeInterval = setInterval(function () {
         secondsLeft--;
-        timerEl.textContent = "Time: " + secondsLeft;
-        // console.log(secondsLeft);
+        if (Number(secondsLeft) < 0) {
+            timerEl.textContent = "Time is Up!"
+            return
+        }
+        setTimerText();
     }, 1000)
 }
 
-// -----User needs to select an answer
-function answerSelect() {
-    
+function setTimerText() {
+    timerEl.textContent = "Time: " + secondsLeft;
 }
+
+
+// -----Quiz needs questions popualted
+function setQuestion() {
+    // Removes existing answers to not clash with new answers buttons
+    blankSlate()
+    showQuestion(questionsAll[currentQuestion])
+}
+
+function blankSlate() {
+    while (answerButtonsEl.firstChild) {
+        answerButtonsEl.removeChild(answerButtonsEl.firstChild)
+
+    }
+}
+
+function showQuestion(questionsAll) {
+    questionEl.innerHTML = questionsAll.question
+    questionsAll.answers.forEach(answer => {
+        const button = document.createElement("button")
+        button.innerText = answer.text
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", answerSelect)
+        answerButtonsEl.appendChild(button)
+    })
+}
+
+// -----User needs to select an answer
+function answerSelect(e) {
+    var selectedAnswer = e.target
+    Array.from(answerButtonsEl.children).forEach(button => {
+        setClass(button, button.dataset.correct)
+    })
+
 // -----Determine if it is correct or not and subtract time (10 seconds) if incorrect
 // -----Display to user if right or wrong
+    if (selectedAnswer.dataset.correct) {
+        selectedAnswerEl.textContent = "Correct!"
+        selectedAnswerEl.classList.remove("hide")
+        score++
+    } else {
+        selectedAnswerEl.textContent = "Wrong!"
+        selectedAnswerEl.classList.remove("hide")
+        secondsLeft = secondsLeft - 10
+    }
+    nextQuestion()
+
+}
+// Moves on to the next question or heads to ending game
+function nextQuestion () {
+    currentQuestion++
+    if (currentQuestion === questionsAll.length) {
+        questionEl.classList.add("hide")
+        blankSlate()
+    } else {
+    setQuestion()
+    }
+}
+
+function setClass(element, correct) {
+    if (correct) {
+        element.classList.add("correct")
+    } else {
+        element.classList.add("wrong")
+    }
+}
+
 // -----When the quiz is finished or time runs out, end the quiz and display the time as the score
 // -----Let users input their initials to track on highscores
 // -----Display highscores with retry quiz and clear highscore options
